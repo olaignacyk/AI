e4fe69f8d0aeab16a2514ebe851a556cb91ce8b0
Ola Ignacyk <olaignacyk@student.agh.edu.pl>
2025-03-06 12:01:10 +0100

    better

--- lab1/ConnectFourFinal.py
+++ lab1/ConnectFourFinal.py
@@ -1,74 +1,109 @@
-from easyAI import TwoPlayerGame, Human_Player, AI_Player, Negamax
-import random
+try:
+    import numpy as np
+except ImportError:
+    print("Sorry, this example requires Numpy installed !")
+    raise
+
+from easyAI import TwoPlayerGame
+
 
 class ClumsyConnectFour(TwoPlayerGame):
-    def __init__(self, players=None):
+    """
+    Clumsy Connect Four - żeton może spaść do sąsiedniej kolumny z 10% szansą.
+    """
+
+    def __init__(self, players, board=None):
         self.players = players
-        self.board = [[' ' for _ in range(7)] for _ in range(6)]
-        self.current_player = 1  # Gracz 1 zaczyna
+        self.board = (
+            board
+            if (board is not None)
+            else (np.array([[0 for i in range(7)] for j in range(6)]))
+        )
+        self.current_player = 1  # player 1 starts.
 
     def possible_moves(self):
-        return [str(c) for c in range(7) if self.board[0][c] == ' ']
+        """Zwraca listę kolumn, do których można wrzucić żeton."""
+        return [i for i in range(7) if (self.board[:, i].min() == 0)]
 
     def make_move(self, column):
-        column = int(column)
-        actual_column = self.probabilistic_column(column)
+        """Wrzucenie żetonu z mechanizmem 'rozjeżdżania'."""
+        column = self.apply_probabilistic_shift(column)
 
-        for row in reversed(range(6)):
-            if self.board[row][actual_column] == ' ':
-                self.board[row][actual_column] = self.current_player_symbol()
-                break
+        line = np.argmin(self.board[:, column] != 0)
+        self.board[line, column] = self.current_player
 
-    def probabilistic_column(self, column):
-        roll = random.random()
-        if roll < 0.1 and column > 0:
+    def apply_probabilistic_shift(self, column):
+        """
+        Z 10% szansą przesuwa żeton w lewo lub w prawo, jeśli to możliwe.
+        """
+        roll = np.random.random()
+        if roll < 0.1 and column > 0:  # 10% szansy na przesunięcie w lewo
             return column - 1
-        elif roll < 0.2 and column < 6:
+        elif roll < 0.2 and column < 6:  # 10% szansy na przesunięcie w prawo
             return column + 1
-        return column
-
-    def current_player_symbol(self):
-        return 'X' if self.current_player == 1 else 'O'
-
-    def unmake_move(self, column):
-        column = int(column)
-        for row in range(6):
-            if self.board[row][column] != ' ':
-                self.board[row][column] = ' '
-                break
+        return column  # 80% na pozostanie w tej samej kolumnie
 
     def show(self):
-        for row in self.board:
-            print("|" + "|".join(row) + "|")
-        print(" " + " ".join(map(str, range(7))))
+        print(
+            "\n"
+            + "\n".join(
+                ["0 1 2 3 4 5 6", 13 * "-"]
+                + [
+                    " ".join([[".", "O", "X"][self.board[5 - j][i]] for i in range(7)])
+                    for j in range(6)
+                ]
+            )
+        )
 
     def lose(self):
-        return self.four_in_a_row(self.current_player_symbol())
+        """Czy obecny gracz przegrał (czy przeciwnik wygrał)."""
+        return find_four(self.board, self.opponent_index)
 
     def is_over(self):
-        return self.possible_moves() == [] or self.lose()
+        """Czy gra się zakończyła."""
+        return (self.board.min() > 0) or self.lose()
 
     def scoring(self):
-        if self.lose():
-            return -100
-        return 0
+        """Ocena stanu gry dla algorytmu Negamax."""
+        return -100 if self.lose() else 0
 
-    def four_in_a_row(self, symbol):
-        for row in range(6):
-            for col in range(4):
-                if all(self.board[row][col+i] == symbol for i in range(4)):
-                    return True
 
-        for row in range(3):
-            for col in range(7):
-                if all(self.board[row+i][col] == symbol for i in range(4)):
+def find_four(board, current_player):
+    """
+    Sprawdza, czy obecny gracz ułożył 4 żetony w linii.
+    """
+    for pos, direction in POS_DIR:
+        streak = 0
+        while (0 <= pos[0] <= 5) and (0 <= pos[1] <= 6):
+            if board[pos[0], pos[1]] == current_player:
+                streak += 1
+                if streak == 4:
                     return True
+            else:
+                streak = 0
+            pos = pos + direction
+    return False
 
-        for row in range(3):
-            for col in range(4):
-                if all(self.board[row+i][col+i] == symbol for i in range(4)):
-                    return True
-                if all(self.board[row+3-i][col+i] == symbol for i in range(4)):
-                    return True
 
-        return False
+POS_DIR = np.array(
+    [[[i, 0], [0, 1]] for i in range(6)]  # poziomo
+    + [[[0, i], [1, 0]] for i in range(7)]  # pionowo
+    + [[[i, 0], [1, 1]] for i in range(1, 3)]  # skos ↘️
+    + [[[0, i], [1, 1]] for i in range(4)]
+    + [[[i, 6], [1, -1]] for i in range(1, 3)]  # skos ↙️
+    + [[[0, i], [1, -1]] for i in range(3, 7)]
+)
+
+if __name__ == "__main__":
+    from easyAI import AI_Player, Negamax
+
+    ai_algo_negamax = Negamax(3)
+
+    # Zmieniamy grę na ClumsyConnectFour
+    game = ClumsyConnectFour([AI_Player(ai_algo_negamax), AI_Player(ai_algo_negamax)])
+    game.play()
+
+    if game.lose():
+        print(f"Player {game.opponent_index} wins.")
+    else:
+        print("Looks like we have a draw.")
